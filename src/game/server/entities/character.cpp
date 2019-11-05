02843ff9cd994eb4d1abe08e85e66dc84d07c26a
AssassinTee <m.winkens@yahoo.de>
2019-11-05 17:20:09 +0100

    fixed some bugs

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -742,7 +742,7 @@ bool CCharacter::TakeDamage(vec2 Force, vec2 Source, int Dmg, int From, int Weap
             }
             GameServer()->CreateSound(GameServer()->m_apPlayers[From]->m_ViewPos, SOUND_HIT, Mask);
         }
-        if(Weapon != WEAPON_GRENADE || (Weapon == WEAPON_GRENADE && Dmg >= 4))
+        if(Weapon != WEAPON_GRENADE || (Weapon == WEAPON_GRENADE && Dmg >= 4))//TODO Settings
         {
             Die(From, Weapon);
 
